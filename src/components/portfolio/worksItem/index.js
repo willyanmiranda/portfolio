7bb8b7b9f7c02bdc092b1c70c3076cf214b71916
willyan miranda <willyanmiranda041@gmail.com>
2023-01-02 22:15:21 -0300

    add noreferrer

--- src/components/portfolio/worksItem/index.js
+++ src/components/portfolio/worksItem/index.js
@@ -1,10 +1,10 @@
 const WorkItems = ({item}) => {
     return(
-        <a href={item.link} target="_blank">
+        <a href={item.link} target="_blank" rel="noreferrer">
             <div id="work-card" key={item.id}>
                 <img src={item.image} id="work-img" />
                 <h3 id="work-title">{item.title}</h3>
-                <a href={item.git} id="work-button" target="_blank">
+                <a href={item.git} id="work-button" target="_blank" rel="noreferrer">
                     Repositório Github <i className="bx bx-right-arrow-alt" id="work-button-icon"></i>
                 </a>
             </div>
