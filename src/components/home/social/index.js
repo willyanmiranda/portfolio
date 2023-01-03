7db38f6b3f416fa7e37c5c8fda1903c1df5948ee
willyan miranda <willyanmiranda041@gmail.com>
2023-01-02 21:59:18 -0300

    fix anchor bug

--- src/components/home/social/index.js
+++ src/components/home/social/index.js
@@ -3,15 +3,15 @@ import React from "react";
 const Social = () => {
     return(
         <div id="home-social">
-            <a href="https://www.instagram.com/lyan_nda/" id="home-social-icon" target="_blank">
+            <a href="https://www.instagram.com/lyan_nda/" id="home-social-icon" target="_blank" rel="help">
                 <i className="uil uil-instagram"></i>
             </a>
 
-            <a href="https://www.linkedin.com/in/willyan-miranda-568182200" id="home-social-icon" target="_blank">
+            <a href="https://www.linkedin.com/in/willyan-miranda-568182200" id="home-social-icon" target="_blank" rel="help">
                 <i class="uil uil-linkedin"></i>
             </a>
 
-            <a href="https://github.com/willyanmiranda" id="home-social-icon" target="_blank">
+            <a href="https://github.com/willyanmiranda" id="home-social-icon" target="_blank" rel="help">
                 <i className="uil uil-github-alt"></i>
             </a>
         </div>
